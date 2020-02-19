c9ebb2cbf125c797bbaeb0fa3c6d4da423989518
Mason <42420694+mcole18@users.noreply.github.com>
2020-02-19 11:48:55 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import {Platform} from 'react-native';
 var port = ':3000';
 var local =
   'http://' + (Platform.OS === 'ios' ? 'localhost' : '10.0.2.2') + port;
-var heal3 = 'https://heal3.poly.asu.edu/web/timelines/home';
-var development = heal3;
+var dev3 = 'https://umm.hi.changeme.plz';
+var development = dev3;
 
 export const Config = {
   URI: development,
