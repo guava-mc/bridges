fa1ab1fb53fdfe4a2dd823bfba906a82278905bd
Mason Cole <mcole18@asu.edu>
2020-03-16 09:53:04 -0700

    update podfile to support new acorn version

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import {Platform} from 'react-native';
 var port = ':3000';
 var local =
   'http://' + (Platform.OS === 'ios' ? 'localhost' : '10.0.2.2') + port;
-var dev3 = 'https://umm.hi.changeme.plz';
+var dev3 = 'https://x.x.x';
 var development = dev3;
 
 export const Config = {
